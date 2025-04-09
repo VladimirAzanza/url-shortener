95968f96a37986513f3b9ed2162a4888e67de624
vlachu <vlachu@anykey.pro>
2025-04-09 23:19:52 +0300

    add small fix

--- internal/server/fiber_server.go
+++ internal/server/fiber_server.go
@@ -22,7 +22,7 @@ func NewFiberServer(urlController *controller.FiberURLController) *fiber.App {
 	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	app.Get("/:id", urlController.HandleGet)
-	app.Post("/", urlController.HandlePost)
+	app.Post("/api/shorten", urlController.HandlePost)
 	return app
 }
 
