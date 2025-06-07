be750b3666b8232cc0edd9c520d063c3e928a50c
vlachu <vlachu@anykey.pro>
2025-06-07 23:36:16 +0300

    add logic for file repo

--- internal/repo/file_repo/file.go
+++ internal/repo/file_repo/file.go
@@ -1,6 +1,7 @@
 package filerepo
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -13,43 +14,59 @@ import (
 )
 
 type FileRepository struct {
-	cfg *config.Config
+	cfg     *config.Config
+	file    *os.File
+	encoder *json.Encoder
+	storage map[string]string
 }
 
-func NewFileRepository(cfg *config.Config) repo.URLRepository {
-	return &FileRepository{
-		cfg: cfg,
+func NewFileRepository(cfg *config.Config) repo.IURLRepository {
+	fileRepo := &FileRepository{
+		cfg:     cfg,
+		storage: make(map[string]string, 0),
 	}
+	fileRepo.initFile()
+	return fileRepo
 }
 
-func (s *FileRepository) SaveRecord(shortID, originalURL string) error {
-	if s.cfg.FileStoragePath == "" {
-		log.Error().Msg("File storage path is empty")
-		return fmt.Errorf("file storage path is empty")
+func (r *FileRepository) initFile() {
+	file, err := os.OpenFile(r.cfg.FileStoragePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to open storage file")
+		return
 	}
+	r.file = file
+	r.encoder = json.NewEncoder(file)
+}
 
+func (r *FileRepository) SaveShortID(ctx context.Context, shortID, originalURL string) error {
 	urlRecord := dto.URLRecord{
 		UUID:        uuid.New().String(),
 		ShortURL:    shortID,
 		OriginalURL: originalURL,
 	}
+	r.storage[shortID] = originalURL
 
-	file, err := os.OpenFile(s.cfg.FileStoragePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to open storage file")
-		return fmt.Errorf("failed to open storage file")
+	if err := r.encoder.Encode(urlRecord); err != nil {
+		return fmt.Errorf("failed to write record: %w", err)
 	}
-	defer file.Close()
+	return nil
+}
 
-	data, err := json.Marshal(urlRecord)
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to Marshal url record")
-		return fmt.Errorf("failed to Marshal url record")
-	}
+func (r *FileRepository) SaveBatchURL(ctx context.Context, shortID, originalURL string) error {
+	return r.SaveShortID(ctx, shortID, originalURL)
+}
 
-	if _, err := file.WriteString(string(data) + "\n"); err != nil {
-		log.Error().Err(err).Msg("Failed to write record")
-		return fmt.Errorf("failed to write record")
+// Implementation for searching in the file (may be inefficient for many records)
+// In a real implementation, consider using a database or an index.
+func (r *FileRepository) GetOriginalURL(ctx context.Context, shortID string) (string, bool, error) {
+	originalURL, ok := r.storage[shortID]
+	return originalURL, ok, nil
+}
+
+func (r *FileRepository) Ping(ctx context.Context) error {
+	if r.file == nil {
+		return fmt.Errorf("file storage not initialized")
 	}
 	return nil
 }
